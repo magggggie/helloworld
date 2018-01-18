db65821fa323cc33ec5bc6afc9b16594ebe2c6f1
Rui wang <ruiwang@RuideMacBook-Pro.local>
2018-01-18 10:37:54 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        }
+    }
+
+    getTodos(){
+
+    }
+
+    getProjects(){
+        this.setState({projects: [
+            {
+                id:uuid.v4(),
+                title:'Business Website',
+                category: 'Web Design'
+            },
+            {
+                id:uuid.v4(),
+                title:'Social App',
+                category: 'Mobile Development'
+            },
+            {
+                id:uuid.v4(),
+                title:'Ecommerce Shopping Cart',
+                category: 'Web Development'
+            },
+        ]});
+    }
+
+    componentWillMount(){
+        this.getProjects();
+        this.getTodos();
+    }
+
+    componentDidMount(){
+
+    }
+
+    handleAddProject(project){
+       let projects = this.state.projects;
+       projects.push(project);
+       this.setState({projects:projects});
+    }
+     handleDeleteProject(id){
+        let projects = this.state.projects;
+        let index = projects.findIndex(x => x.id === id);
+        projects.splice(index,1);
+        this.setState({projects:projects});
+     }
+  render() {
+    return (
+      <div className="App">
+    <AddProject addProject={this.handleAddProject.bind(this)}/>
+          <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
